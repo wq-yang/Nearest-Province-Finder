687d10bc6f241f9258d4f5eea3bc16961e3901b5
wqyang <wqyang@bu.edu>
2021-12-06 17:38:19 -0500

    bug fix

--- findKNearst.cpp
+++ findKNearst.cpp
@@ -10,14 +10,15 @@
 using namespace std;
 const int MAX = 1<<22;
 const int K = 7;
+const double R = 6371000;
 int RECORDS;
-struct County {
-    string state, name;
+struct Place {
+    string state, county;
     double lat, lon; // latitude and longitude
     double minLat, minLon, maxLat, maxLon;// min and max coordinates among descendants
     int left, right; // children id
-} counties[MAX];
-// find the nearest K counties for each query. pair: { distance, id }
+} places[MAX];
+// find the nearest K places for each query. pair: { distance, id }
 priority_queue<pair<int, int>, vector<pair<int, int> >, less<pair<int, int> > > pq;
 inline bool validLat(int lat) { return -90<=lat && lat<=90; }
 inline bool validLon(int lon) { return -180<=lon && lon<=180; }
@@ -27,7 +28,7 @@ inline double sq_dist(const double lat1, const double lon1, const double lat2, c
     return x*x+y*y;
 }
 // heuristic function. the min distance between the queired point and the rectangle that subtree locates
-inline double h(const County& c, const double lat, const double lon) {
+inline double h(const Place& c, const double lat, const double lon) {
     double x = lon < c.minLon ? c.minLon - lon
             : c.maxLon < lon ? lon - c.maxLon : 0;
     double y = lat < c.minLat ? c.minLat - lat
@@ -53,46 +54,46 @@ void readFile(char *infile) {
     int i=0;
     while (i<MAX) {
         if (!getline(fin, line, '\t')) break;
-        counties[i].state = line;
+        places[i].state = line;
         if (!getline(fin, line, '\t')) break;
-        counties[i].name = line;
+        places[i].county = line;
         try {
             if (!getline(fin, line, '\t')) break;
-            counties[i].lat = stod(line);
+            places[i].lat = stod(line);
         } catch(const std::exception& e) {
             cout << i << ": " << line << endl;
         }
         try {
             if (!getline(fin, line)) break;
-            counties[i].lon = stod(line);
+            places[i].lon = stod(line);
         } catch(const std::exception& e) {
             cout << i << ": " << line << endl;
         }
         # if 0
-        if (!validLat(counties[i].lat) || !validLon(counties[i].lon)) {
-            cout << "invalid: i=" << i << ", name=" << counties[i].name
-            << ", lat = " << counties[i].lat
-            << ", lon = " << counties[i].lon << endl;
+        if (!validLat(places[i].lat) || !validLon(places[i].lon)) {
+            cout << "invalid: i=" << i << ", county=" << places[i].county
+            << ", lat = " << places[i].lat
+            << ", lon = " << places[i].lon << endl;
         }
         # endif
         ++i;
     }
     RECORDS = i;
 }
-void maintain(County& c) {
+void maintain(Place& c) {
     c.minLat = c.maxLat = c.lat;
     c.minLon = c.maxLon = c.lon;
     if (c.left != -1) {
-        c.minLat = fmin(c.minLat, counties[c.left].minLat);
-        c.minLon = fmin(c.minLon, counties[c.left].minLon);
-        c.maxLat = fmax(c.maxLat, counties[c.left].maxLat);
-        c.maxLon = fmax(c.maxLon, counties[c.left].maxLon);
+        c.minLat = fmin(c.minLat, places[c.left].minLat);
+        c.minLon = fmin(c.minLon, places[c.left].minLon);
+        c.maxLat = fmax(c.maxLat, places[c.left].maxLat);
+        c.maxLon = fmax(c.maxLon, places[c.left].maxLon);
     }
     if (c.right != -1) {
-        c.minLat = fmin(c.minLat, counties[c.right].minLat);
-        c.minLon = fmin(c.minLon, counties[c.right].minLon);
-        c.maxLat = fmax(c.maxLat, counties[c.right].maxLat);
-        c.maxLon = fmax(c.maxLon, counties[c.right].maxLon);
+        c.minLat = fmin(c.minLat, places[c.right].minLat);
+        c.minLon = fmin(c.minLon, places[c.right].minLon);
+        c.maxLat = fmax(c.maxLat, places[c.right].maxLat);
+        c.maxLon = fmax(c.maxLon, places[c.right].maxLon);
     }
 }
 // build balanced kd-tree.
@@ -101,38 +102,38 @@ int build(int l, int r) {
     int mid = l + (r-l)/2;
     double mean1=0, mean2=0, var1=0, var2=0;
     for (int i=l; i<=r; ++i) {
-        mean1 += counties[i].lat;
-        mean2 += counties[i].lon;
+        mean1 += places[i].lat;
+        mean2 += places[i].lon;
     }
     mean1 /= r-l+1;
     mean2 /= r-l+1;
     for (int i=l; i<=r; ++i) {
-        var1 += (counties[i].lat-mean1) * (counties[i].lat-mean1);
-        var2 += (counties[i].lon-mean2) * (counties[i].lon-mean2);
+        var1 += (places[i].lat-mean1) * (places[i].lat-mean1);
+        var2 += (places[i].lon-mean2) * (places[i].lon-mean2);
     }
     // cut at the dimension with greatest variance
     if (var1 >= var2) {
-        nth_element(counties+l, counties+mid, counties+r+1, [](County &c1, County &c2) { return c1.lat < c2.lat; });
+        nth_element(places+l, places+mid, places+r+1, [](Place &c1, Place &c2) { return c1.lat < c2.lat; });
     } else {
-        nth_element(counties+l, counties+mid, counties+r+1, [](County &c1, County &c2) { return c1.lon < c2.lon; });
+        nth_element(places+l, places+mid, places+r+1, [](Place &c1, Place &c2) { return c1.lon < c2.lon; });
     }
-    counties[mid].left = build(l, mid-1);
-    counties[mid].right = build(mid+1, r);
-    maintain(counties[mid]);
+    places[mid].left = build(l, mid-1);
+    places[mid].right = build(mid+1, r);
+    maintain(places[mid]);
     return mid;
 }
 void query(int l, int r, int lat, int lon) {
     if (l > r) return;
     int mid = l + (r-l)/2;
-    County cur = counties[mid];
+    Place cur = places[mid];
     double sqd = sq_dist(cur.lat, cur.lon, lat, lon);
     if (pq.top().first > sqd) {
         if (pq.size() >= K) pq.pop();
         pq.push({ sqd, mid });
     }
     // search the left and right subtree heuristically
-    double sqdl = cur.left == -1 ? INFINITY : h(counties[cur.left], lat, lon),
-        sqdr = cur.right == -1 ? INFINITY : h(counties[cur.right], lat, lon);
+    double sqdl = cur.left == -1 ? INFINITY : h(places[cur.left], lat, lon),
+        sqdr = cur.right == -1 ? INFINITY : h(places[cur.right], lat, lon);
     if (sqdl < sqdr && sqdl < pq.top().first) { // search left first, then right if necessary
         query(l, mid-1, lat, lon);
         if (sqdr < pq.top().first) query(mid+1, r, lat, lon);
@@ -155,11 +156,16 @@ int main(int argc, char *argv[]) {
         cout << "\033[;33mQuerying { latitude=" << lat << ", longitude=" << lon << " }...\033[0m\n";
         pq.push({INFINITY, -1});
         query(0, RECORDS-1, lat, lon);
-        cout << "The " << K << " nearest counties are: ";
+        cout << "The " << K << " nearest places are:\n";
         while (!pq.empty()) {
             int id = pq.top().second;
-            if (id != -1) cout << counties[id].name;
-            if (pq.size() > 1) cout << ", ";
+            double dist = pq.top().first;
+            if (id != -1) {
+                cout << "    " << places[id].county << ", " << places[id].state
+                    << "(" << places[id].lat << ", " << places[id].lon << "), ";
+                cout << "distance = " << sqrt(dist)*R << "km";
+            }
+            if (pq.size() > 1) cout << ",\n";
             else cout << ".\n";
             pq.pop();
         }
