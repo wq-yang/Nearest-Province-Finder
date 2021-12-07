d9b4583d3e1be77b89ef7fa38cb3d0ef0e3784fe
wqyang <wqyang@bu.edu>
2021-12-07 02:20:56 -0500

    add majority voting to guess the county

--- findKNearst.cpp
+++ findKNearst.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <queue>
 #include <string>
+#include <unordered_map>
 #include <utility>
 #include <vector>
 using namespace std;
@@ -149,35 +150,53 @@ void query(int l, int r, double phi, double lam) {
 int main(int argc, char *argv[]) {
     cout << "\033[1;33mLoading file...\033[0m\n";
     readFile(argv[1]);
-    cout << "\033[1;36mPlease input K...\033[0m";
+    cout << "\033[1;36mPlease input K: \033[0m";
     cin >> K;
     cout << "\033[1;33mPreprocessing...\033[0m\n";
     build(0, RECORDS-1);
     while (true) {
         double lat, lon, phi, lam;
-        cout << "\033[1;36mPlease input latitude and longitude, separated by space... To quit, press Ctrl+C\033[0m\n";
+        cout << "\033[1;36mPlease input latitude and longitude, separated by space.  To quit, press Ctrl+C\033[0m\n";
         cin >> lat >> lon;
         phi = degToRad(lat);
         lam = degToRad(lon);
         if (!validLat(lat) || !validLon(lon)) {
-            cout << "\033[1;31mInvalid input! Please try Again...\033[0m\n";
+            cout << "\033[1;31mInvalid input! Please try Again.033[0m\n";
             continue;
         }
         cout << "\033[1;33mQuerying { latitude=" << lat << ", longitude=" << lon << " }...\033[0m\n";
         pq.push({INFINITY, -1});
         query(0, RECORDS-1, phi, lam);
-        cout << "The " << K << " nearest places are:\n";
-        int i=0;
-        while (!pq.empty() && i++<K) {
-            int id = pq.top().second;
-            double dist = pq.top().first;
-            if (id != -1) {
-                cout << "    " << places[id].county << ", " << places[id].state
-                    << "(" << places[id].lat << ", " << places[id].lon << "), ";
-                cout << "distance = " << sqrt(dist)*R << "km";
-            }
-            cout << endl;
+
+        // get nearest places
+        vector<pair<double, int> > nearest;
+        while (!pq.empty()) {
+            nearest.push_back({pq.top().first, pq.top().second});
             pq.pop();
+        }
+        // majority voting to find the state and county
+        unordered_map<string, int> countyToCnt;
+        for (int i=nearest.size()-1; i>=int(nearest.size()-5); --i) {
+            int idx = nearest[i].second;
+            string loc = places[idx].county + ", " + places[idx].state;
+            countyToCnt[loc]++;
+        }
+        unordered_map<string, int>::iterator kv;
+        for (kv = begin(countyToCnt); kv != end(countyToCnt); kv = next(kv)) {
+            if (kv->second > 2) {
+                cout << "It might be located at \033[1;31m" << kv->first << "\033[0m\n";
+                break;
+            }
+        }
+        if (kv == end(countyToCnt)) cout << "No majority, failed to guess the location.\n";
+        // print K nearest reference points
+        cout << "The " << K << " nearest places are:\n";
+        for (int i=nearest.size()-1; i>=int(nearest.size()-K); --i) {
+            int idx = nearest[i].second;
+            double dist = nearest[i].first;
+            cout << "\t" << places[idx].county << ", " << places[idx].state
+                << "(" << places[idx].lat << ", " << places[idx].lon << "), "
+                << "distance = " << sqrt(dist)*R << "km" << endl;
         }
     }
 }
